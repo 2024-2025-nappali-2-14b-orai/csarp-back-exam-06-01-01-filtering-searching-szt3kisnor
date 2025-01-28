69329dd6c43acf03cc19bb53aafec76903fc245e
szt3kisnor <szt3kisnor@vasvari.org>
2025-01-28 09:00:06 +0100

    interface GetNumberOfStudnet

--- Kreata.Backend/Repos/IStudentRepo.cs
+++ Kreata.Backend/Repos/IStudentRepo.cs
@@ -1,6 +1,7 @@
 ﻿using Kreata.Backend.Repos.Base;
 using Kreta.Shared.Enums;
 using Kreta.Shared.Models;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Kreata.Backend.Repos
 {
@@ -8,5 +9,6 @@ namespace Kreata.Backend.Repos
     {
         public Task<Student> GetByNameAsync(string firstName, string lastName);
         public Task<List<Student>> GetStudentByClass(int schoolYear, SchoolClassType schoolClassType);
+        public Task<int> GetNumberOfStudnet();
     }
 }
